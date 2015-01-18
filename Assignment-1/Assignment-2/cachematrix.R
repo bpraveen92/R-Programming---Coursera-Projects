259eb641a4251c6720641d6b8d315bf3eda85bfc
Praveen Balasubramanian <bpraveen92@gmail.com>
2015-01-18 13:56:35 -0600

    cachematrix

---
+++ Assignment-1/Assignment-2/cachematrix.R
@@ -0,0 +1,53 @@
+## Creates a special "matrix" object that can cache its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+    # i will store the inverse
+    inv <- NULL
+
+    # set should be used to alter the matrix
+    # it invalidates the cache
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+
+    # get simply returns the raw matrix
+    get <- function() {
+        x
+    }
+
+    # setinv sets the inv variable
+    # should be used only by cacheSolve
+    setinv <- function(i) {
+        inv <<- i
+    }
+
+    # getinv gets the cached inverse
+    getinv <- function() {
+        inv
+    }
+
+    # return the special matrix
+    list(set = set,
+         get = get,
+         setinv = setinv,
+         getinv = getinv)    
+}
+cacheSolve <- function(x, ...) {
+    # get the cached inverse
+    inv <- x$getinv()
+
+    if(!is.null(inv)) {
+        # if the inverse if actually cached, just return it
+        message("getting cached inverse")
+        return(inv)
+    }
+
+    # otherwise, calculate the inverse and cache it
+    matr <- x$get()
+    inv <- solve(matr, ...)
+    x$setinv(inv)
+
+    return(inv)
+}
+
